4404383ca9cb111ca97ca64c912bbb3ebba65dbe
Ali Osman Zengin <aliosmanzengin0212@gmail.com>
2024-07-10 17:15:51 +0200

    add full name

--- src/main/java/org/example/springexercise/Greeter.java
+++ src/main/java/org/example/springexercise/Greeter.java
@@ -1,33 +1,40 @@
 package org.example.springexercise;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
-public class Greeter {
-    private GreetingFormatter formatter;
+@Scope("prototype")
+public class Greeter implements InitializingBean {
+
+    @Value("${title}")
+    private String title;
+
+    @Value("${name}")
     private String name;
 
+    private GreetingFormatter formatter;
+    private String fullName;
+
     @Autowired
-    public Greeter(GreetingFormatter formatter) {
+    public void setFormatter(GreetingFormatter formatter) {
         this.formatter = formatter;
     }
 
-    @Value("${name:World}") //injects the value from application.properties or default value passed
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.fullName = title + name;
     }
-
 
     public void greet(String name) {
         System.out.println(formatter.format(name));
     }
 
     public void greet() {
-        System.out.println(formatter.format(this.name));
+        System.out.println(formatter.format(this.fullName));
     }
 
 }
